c21dc0510ae89fa9d74f166dfc49365d3736fc01
Juan Alberto Resendiz Arteaga <jresendiz@nearsoft.com>
2016-10-19 01:32:53 -0500

    Addding clustering config and commenting unused deployed verticle

--- src/main/groovy/com/nearsoft/jresendiz/vertxworkshop/eventbus/JSProducer.js
+++ src/main/groovy/com/nearsoft/jresendiz/vertxworkshop/eventbus/JSProducer.js
@@ -5,4 +5,4 @@ var eventBus = vertx.eventBus();
 vertx.setPeriodic(500, function (value) {
     eventBus.send("HelloWorldChannel", "Hello World from JS Producer");
 });
-vertx.deployVerticle("./JSConsumer.js");
+// vertx.deployVerticle("./JSConsumer.js");
